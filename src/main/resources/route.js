6b67690ae697288f750bdf28643f50ef4c40473d
lburgazzoli <lburgazzoli@gmail.com>
2019-05-06 22:41:49 +0200

    update exaple

--- src/main/resources/route.js
+++ src/main/resources/route.js
@@ -1,4 +1,5 @@
 from('timer:js?period=1s')
     .setBody().constant('test')
+    .setHeader('test').constant('test')
     .process(e => e.getMessage().setBody('test'))
     .to('log:js?showAll=false&multiline=false')
